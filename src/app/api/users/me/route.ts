4c33fe256bc96f4747ca4f6eaf063f4c2bff8dba
nameerakhter <akhtarnameer@gmail.com>
2024-07-09 20:06:11 +0530

    completed backend

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,2 +1,16 @@
+import UserModel from "@/app/models/User";
 import dbConnect from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
 import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(request: NextRequest){
+    // Extract data from token
+    const userId = await getDataFromToken(request)
+    const user = await UserModel.findOne({_id: userId}).select("-password")
+    return NextResponse.json({
+        message: "User data fetched successfully",
+        data: user
+    })
+
+}
