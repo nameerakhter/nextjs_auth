da173d44da8a469d6f3b38ab072589b11bdcc796
nameerakhter <akhtarnameer@gmail.com>
2024-07-10 00:11:43 +0530

    starting with front-end

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -2,19 +2,18 @@ import dbConnect from "@/dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function GET(request: NextRequest){
-    await dbConnect()
+export async function GET(){
     try {
         const response = NextResponse.json({
             message: "Logged out successfully",
             success: true
         })
+        console.log(response)
 
         response.cookies.set("token", "",{
             httpOnly: true,
             expires: new Date(0)
         },)
-
         return response
     } catch (error:any) {
         return Response.json({ error: error.message }, { status: 500 });
